2a7aba71874505975ac4a254bad00384b3207864
Jain <zaiin4050@gmail.com>
2020-05-12 17:20:44 +0900

    basic model

--- keras/keras01.py
+++ keras/keras01.py
@@ -12,7 +12,8 @@ model.add(Dense(1, input_dim=1,activation='relu' ))
 
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x, y, epochs =500, batch_size=1)
+model.fit(x, y, epochs =500, batch_size=1)  # epochs : 훈련횟수, 
+                                            # batch_size : 넘겨주는 데이터 샘플의 size, defalut값 = 32
 
 loss, acc = model.evaluate(x, y, batch_size=1)
 
