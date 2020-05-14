0184634de2e3cdac266652b3e362d22a2ece9e84
Jain <zaiin4050@gmail.com>
2020-05-14 19:38:02 +0900

    range를 이용한 validation 나누기(python slicig)

--- keras/keras11_split.py
+++ keras/keras11_split.py
@@ -3,9 +3,26 @@ import numpy as np
 x = np.array(range(1, 101))
 y = np.array(range(101, 201))  # train, test, validation 나누기(직접 손으로)
 
+# range(a, b) : a에서 (b-1)까지 생성됨
+#             : a값 설정 안할 시 = 0
+# 만들어진 후 0부터 시작되는 순서 부여됨 
+
 x_train = x[:60]  # python시퀀스 자료형 슬라이스 참조
 x_val = x[60:80]
 x_test = x[80:]
+
+# 실제 : 1 2 3 4 5 6 7 8 ~ 58 59 60 61 62 ~ 78 79 80 81 82 ~ 98 99 100
+# 배열 : 0 1 2 3 4 5 6 7 ~ 57 58 59 60 61 ~ 77 78 79 80 81 ~ 97 98  99   
+
+# x[:60] = 1 ~ 60
+#   배열 : 0 ~ 59까지 출력
+# 배열의 순서로 따짐 : 0부터 (60 - 1)번 째 까지 
+
+# x[60:80] = 61 ~ 80
+#     배열 : 60 ~ 79 (80 - 1)            
+
+# x[80:] = 81 ~ 100
+#   배열 : 80 ~ 99
 
 y_train = y[:60]
 y_val = y[60:80]
@@ -15,21 +32,24 @@ print(x_train)
 print(x_val )
 print(x_test )
 
-
+'''
 #2. 모델구성
 from keras.models import Sequential
 from keras.layers import Dense
 model = Sequential()
 
 model.add(Dense(5, input_dim = 1 ))
-model.add(Dense(10))
-model.add(Dense(15))
 model.add(Dense(20))
-model.add(Dense(30))
-model.add(Dense(70))
+model.add(Dense(50))
+model.add(Dense(100))
+model.add(Dense(150))
+model.add(Dense(100))
+model.add(Dense(200))
+model.add(Dense(300))
+model.add(Dense(230))
+model.add(Dense(100))
+model.add(Dense(80))
 model.add(Dense(40))
-model.add(Dense(20))
-model.add(Dense(10))
 model.add(Dense(1))
 
 
@@ -59,5 +79,4 @@ print("RMSE : ", RMSE(y_test, y_predict))
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print("R2 : ", r2)
-
- 
+'''
