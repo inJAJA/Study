991203acb9f93ec12c703cc976a38be582f3dd1f
Jain <zaiin4050@gmail.com>
2020-05-15 15:33:16 +0900

    RMSE

--- keras/keras07_RMSE.py
+++ keras/keras07_RMSE.py
@@ -51,6 +51,8 @@ def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))   # sprt() : root를 씌어줌
 print("RMSE : ", RMSE(y_test, y_predict))
 
+'''
 # RMSE (Root Mean Squared Error, 평균 제곱근 오차) 
 # : root[sigma(실제값 - 예측값)^2 /n]
 # : MSE에 비해 에러값의 크기가 실제 값에 비례한다.(제곱된 것에 루트를 씌었기 때문)
+'''
