da0f470ed0708cb6dd2631b24a66fe8856ccd609
Jain <zaiin4050@gmail.com>
2020-05-18 10:49:45 +0900

    x1,y1 수정

--- keras/keras21_ensemble.py
+++ keras/keras21_ensemble.py
@@ -42,7 +42,7 @@ dense1 = Dense(10, activation = 'relu')(dense1)
 dense1 = Dense(10, activation = 'relu')(dense1)
 
 
-output1 = Dense(1)(dense1)   # output layer
+output1 = Dense(3)(dense1)   # output layer
 
 model = Model(inputs = input1, outputs= output1) # 함수형 모델이라고 정의 : 시퀀스 모델의 경우 ex) model = Sequential()
 
@@ -50,31 +50,31 @@ model.summary()
 
 #3. 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])  
-model.fit(x_train, y_train, epochs =80, batch_size =1,
+model.fit(x1_train, y1_train, epochs =80, batch_size =1,
         # validation_data = (x_val, y_val)
           validation_split= 0.25, verbose=1 
 )
 
 #4. 평가,예측
-loss, mse = model.evaluate(x_test, y_test, batch_size =1) 
+loss, mse = model.evaluate(x1_test, y1_test, batch_size =1) 
 print("loss : ", loss)
 print("mse : ", mse)
 
 # y_pred = model.predict(x_pred)  #눈으로 보기 위한 예측값
 # print("y_pred : ", y_pred)
 
-y_predict = model.predict(x_test)  
+y1_predict = model.predict(x1_test)  
 print(y_predict)
 
 # RMSE 구하기
 from sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
-print("RMSE : ", RMSE(y_test, y_predict))
+print("RMSE : ", RMSE(y1_test, y1_predict))
 
 # R2 구하기
 from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict)
+r2 = r2_score(y1_test, y1_predict)
 print("R2 : ", r2)
 
 
