9c53a0c046aaabbaec110de42f08b77479a49da2
Jain <zaiin4050@gmail.com>
2020-05-18 18:33:25 +0900

    주석

--- keras/keras22_ensemble2.py
+++ keras/keras22_ensemble2.py
@@ -25,8 +25,8 @@ x2_train, x2_test, y2_train, y2_test = train_test_split(
     train_size =0.8                                     
     )    
 
-y3_train, y3_test = train_test_split(
-    y3, shuffle = False,
+y3_train, y3_test = train_test_split(                 # x, y가 꼭 쌍으로 묶일 필요 없음
+    y3, shuffle = False,                              # 3개의 random_state가 같은게 좋음
     train_size = 0.8
 )
 
@@ -39,7 +39,7 @@ from keras.layers import Dense, Input
 input1 = Input(shape =(2, ))           
 dense1_1 = Dense(7, activation = 'relu')(input1)
 dense1_2 = Dense(10, activation = 'relu' )(dense1_1)
-dense1_2 = Dense(5, activation = 'relu')(dense1_1)
+dense1_2 = Dense(5, activation = 'relu')(dense1_2)
 
    
 
@@ -47,7 +47,7 @@ dense1_2 = Dense(5, activation = 'relu')(dense1_1)
 input2 = Input(shape =(2, )) 
 dense2_1 = Dense(8, activation = 'relu')(input2) 
 dense2_2 = Dense(12, activation = 'relu')(dense2_1)
-dense2_2 = Dense(4, activation = 'relu')(dense2_1)
+dense2_2 = Dense(4, activation = 'relu')(dense2_2)
   
 
 ######### 모델 병합#########
@@ -55,20 +55,21 @@ from keras.layers.merge import concatenate
 merge1 = concatenate([dense1_2, dense2_2])   # list형태로 묶임 
 
 middle1 = Dense(10)(merge1)
-middle1 = Dense(10)(middle1) 
 middle1 = Dense(7)(middle1)
+middle1 = Dense(3)(middle1)
+
 
 ######### output 모델 구성 ###########
 
-output1 = Dense(30)(middle1)   
+output1 = Dense(25)(middle1)   
 output1_2 = Dense(15)(output1)
 output1_3 = Dense(2)(output1_2)  
 
-output2 = Dense(25)(middle1)   
+output2 = Dense(20)(middle1)   
 output2_2 = Dense(12)(output2)
 output2_3 = Dense(2)(output2_2) 
 
-output3 = Dense(20)(middle1)
+output3 = Dense(10)(middle1)
 output3_2 = Dense(9)(output3)
 output3_3 = Dense(2)(output3_2)
 
@@ -82,9 +83,9 @@ model.summary()
 #3. 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])  
 model.fit([x1_train, x2_train], 
-          [y1_train, y2_train, y3_train], epochs =100, batch_size =1,
+          [y1_train, y2_train, y3_train], epochs =300, batch_size =1,
         # validation_data = (x_val, y_val)
-        # validation_split= 0.25, verbose=1
+        validation_split= 0.25, verbose=1
 )
 
 #4. 평가,예측
