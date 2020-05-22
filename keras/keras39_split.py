d3e1a511b29fda8dba3aa1d795ae47c654796a83
Jain <zaiin4050@gmail.com>
2020-05-22 19:57:15 +0900

    값 수정

--- keras/keras39_split.py
+++ keras/keras39_split.py
@@ -4,7 +4,7 @@ from keras.layers import Dense, LSTM
 
 #1. 데이터
 a = np.array(range(1, 11))                     # a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    (10, )
-size = 65                                      #      0  1  2  3  4  5  6  7  8   9 
+size = 5                                       #      0  1  2  3  4  5  6  7  8   9 
 
 def split_x(seq, size):
     aaa = []
