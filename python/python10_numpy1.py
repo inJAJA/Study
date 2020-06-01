c08855f13e4ea4328ebce1faffe2e8f806e610fd
Jain <zaiin4050@gmail.com>
2020-06-01 20:51:11 +0900

    2020.06.01

---
+++ python/python10_numpy1.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+# ndarray = 배열을 고속으로 처리하는 class
+
+# np.array( )
+a = np.array([1, 2, 3])
+print(a)                                              # [1 2 3]
+
+
+""" Tensor """
+# scalars : 1차원
+a_1d = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+print(a_1d.shape)                                    # (8,)
+
+# matrix : 2차원
+a_2d = np.array([[1, 2, 3, 4],[5, 6, 7, 8]])        
+print(a_2d.shape)                                    # (2, 4)
+
+# vector : 3차원
+a_3d = np.array([[[1, 2], [3, 4]], [[5, 6],[7, 8]]])
+print(a_3d.shape)                                    # (2, 2, 2)
+
+# tensor : 4차원
+a_4d = np.array([[[[1],[2]]],[[[3],[4]]]])
+print(a_4d.shape)                                    # (2, 1, 2, 1)
+
+
+''' numpy연산 '''
+# 같은 위치에 있는 요소끼리 계산됌
+a = np.array([1, 2, 3])
+b = np.array([1, 2, 3])
+
+print(a + b)                                         # [2 4 6]
+print(a - b)                                         # [0 0 0]
+print(a * b)                                         # [1 4 9]
+print(a / b)                                         # [1. 1. 1.]
+print(a ** 3) # 3승                                  # [ 1  8 27]
+
+# list
+c = [1, 2, 3]
+d = [1, 2, 3]
+
+print(c + d)                                         # [1, 2, 3, 1, 2, 3]
+# print(c - d)
+# print(c * d)
+# print(c % d)                                       # error
+
+
+''' index 참조, slice '''
+a = np.arange(10)
+print(a)                                             # [0 1 2 3 4 5 6 7 8 9]
+
+a1 = range(10)
+print(a1)                                            # range(0, 10)
