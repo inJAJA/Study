14765e6b507cd65b595bcffbbc9d2d51521afddd
Jain <zaiin4050@gmail.com>
2020-06-19 14:06:14 +0900

    2020.06.19

---
+++ ml/m22_xgb.py
@@ -0,0 +1,56 @@
+## 과적합 방지
+# 1. 훈련데이터량을 늘린다.
+# 2. feature수를 줄인다.
+# 3. regularization
+                
+from xgboost import XGBRegressor, plot_importance  
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# 회귀 모델
+dataset = load_boston()
+x = dataset.data
+y = dataset.target
+
+print(x.shape)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8,
+                                                    shuffle = True, random_state = 66)
+
+# 트리구조 : 결측치 제거, 데이터 전처리 필요 없음
+
+# parameter
+n_estimator = 100          # 나무의 개수 ( 앙상블 )
+learning_rate = 0.01       # 학습률 : 이걸 건드는 것이 제일 효과가 크다. / default = 0.01
+colsample_bytree = 0.9     # 각 트리마다의 feature 샘플링 비율 :0.6 ~ 0.9  / max 1  // 샘플 = 전체 나무 ( dropout과 유사 )
+colsample_bylevel = 0.9    # 샘플을 어느정도 사용할 것인가.     : 0.6 ~ 0.9 / max 1
+
+max_depth = 5              # 큰 영향을 주지 않는다.
+n_jobs = -1                # 딥러닝 아닌 경우에 무조건 ' -1 ' 쓰기 / 딥러닝에서는 잘 터짐
+
+model = XGBRegressor(max_depth = max_depth, learning_rate = learning_rate,
+                    n_estimator = n_estimator, n_jobs = n_jobs,
+                    # colsample_bytree = colsample_bytree,
+                    colsample_bylevel = colsample_bylevel)
+
+model.fit(x_train, y_train)
+
+score = model.score(x_test, y_test)
+print('점수 :', score)
+
+print(model.feature_importances_)
+
+print("========================")
+print(model.best_estimator_)
+print("========================")
+print(model.best_params_)
+
+
+# feature_importances_ 를 graph로 보여줌
+plot_importance(model)
+plt.show()
+
+# f0 : 첫번째 feature  
+# fn : n번째 feature
