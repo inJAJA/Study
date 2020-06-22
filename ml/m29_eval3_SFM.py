0867d0057192ae528c44952039736877ff3a7e89
Jain <zaiin4050@gmail.com>
2020-06-22 11:52:44 +0900

    2020.06.22

--- ml/m29_eval3_SFM.py
+++ ml/m29_eval3_SFM.py
@@ -16,9 +16,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8,
 
 model = XGBClassifier(objective='multi:softmax', n_estimators = 100, learning_rate = 0.05, n_jobs = -1)
 
-model.fit(x_train, y_train, verbose = True, eval_metric= ['mlogloss', 'merror'],
-                            eval_set= [(x_train, y_train),(x_test, y_test)],
-                            early_stopping_rounds= 20)
+model.fit(x_train, y_train)
 
 threshold = np.sort(model.feature_importances_)
 
