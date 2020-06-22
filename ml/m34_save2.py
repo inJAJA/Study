c83aa4877d640fde9479cc70e76390a29ac0490f
Jain <zaiin4050@gmail.com>
2020-06-22 15:33:24 +0900

    2020.06.22

---
+++ ml/m34_save2.py
@@ -0,0 +1,44 @@
+from xgboost import XGBClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = load_breast_cancer(return_X_y=True)
+
+print(x.shape)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8,
+                                                    shuffle = True, random_state = 66)
+
+model = XGBClassifier(n_estimators = 100, learning_rate = 0.05, n_jobs = -1)
+
+model.fit(x_train, y_train)
+
+threshold = np.sort(model.feature_importances_)
+
+for thres in threshold:
+    selection = SelectFromModel(model, threshold = thres, prefit = True)
+    
+    select_x_train = selection.transform(x_train)
+    select_x_test = selection.transform(x_test)
+
+    selection_model = XGBClassifier(n_estimators = 100, learning_rate = 0.05, n_jobs = -1) 
+
+    selection_model.fit(select_x_train, y_train, verbose= False, eval_metric= ['logloss', 'error'],
+                                        eval_set= [(select_x_train, y_train), (select_x_test, y_test)],
+                                        early_stopping_rounds= 20)
+
+    y_pred = selection_model.predict(select_x_test)
+    acc = accuracy_score(y_test, y_pred)
+     
+    print("Thresh=%.3f, n = %d, ACC : %.2f%%" %(thres, select_x_train.shape[1], acc*100.0))
+
+    # result = selection_model.evals_result()
+    # print("eval's result : ", result)
+
+    model.save_model("./model/xgb_save/thresh=%.3f-acc=%.2f"%(thres, acc))
+
---
+++ ml/m34_save3.py
@@ -0,0 +1,50 @@
+from xgboost import XGBClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+x, y = load_iris(return_X_y=True)
+
+print(x.shape)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8,
+                                                    shuffle = True, random_state = 66)
+
+model = XGBClassifier(objective='multi:softmax', n_estimators = 100, learning_rate = 0.05, n_jobs = -1)
+
+model.fit(x_train, y_train)
+
+threshold = np.sort(model.feature_importances_)
+
+start = time.time()
+
+for thres in threshold:
+    selection = SelectFromModel(model, threshold = thres, prefit = True)
+    
+    select_x_train = selection.transform(x_train)
+    select_x_test = selection.transform(x_test)
+
+    selection_model = XGBClassifier(objective='multi:softmax', n_estimators = 100, learning_rate = 0.05, n_jobs = -1) 
+
+    selection_model.fit(select_x_train, y_train, verbose= False, eval_metric= ['mlogloss', 'merror'],
+                                        eval_set= [(select_x_train, y_train), (select_x_test, y_test)],
+                                        early_stopping_rounds= 20)
+
+    y_pred = selection_model.predict(select_x_test)
+    acc = accuracy_score(y_test, y_pred)
+     
+    print("Thresh=%.3f, n = %d, ACC : %.2f%%" %(thres, select_x_train.shape[1], acc*100.0))
+
+    # result = selection_model.evals_result()
+    # print("eval's result : ", result)
+    
+    model.save_model("./model/xgb_save/thresh=%.3f-acc=%.2f"%(thres, acc))
+
+end = time.time() - start
+print(" 걸린 시간 :", end)
+
+
