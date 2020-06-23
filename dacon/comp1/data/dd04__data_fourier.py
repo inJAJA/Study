f852bef73cb2eb89323abc3ac06f94753ae7dabd
Jain <zaiin4050@gmail.com>
2020-06-23 20:32:31 +0900

    수정중

--- dacon/comp1/data/dd04__data_fourier.py
+++ dacon/comp1/data/dd04__data_fourier.py
@@ -15,11 +15,17 @@ train_dst = np.load('./dacon/comp1/data/train_dst.npy')
 test_dst = np.load('./dacon/comp1/data/test_dst.npy')
 
 # fourier
-train_scr_fft = np.fft.fft(train_scr, axis = 1)/ train_scr.shape[1]
-test_scr_fft = np.fft.fft(test_scr, axis = 1) / test_scr.shape[1]
+n1_1 = train_scr.shape[1]
+n1_2 = test_scr.shape[1]
 
-train_dst_fft = np.fft.fft(train_dst, axis = 1)/ train_dst.shape[1]
-test_dst_fft = np.fft.fft(test_dst, axis = 1)/ test_dst.shape[1]
+n2_1 = train_dst.shape[1]
+n2_2 = test_dst.shape[1]
+
+train_scr_fft = np.fft.fft(train_scr, axis = 1)/ n1_1
+test_scr_fft = np.fft.fft(test_scr, axis = 1) / n1_2
+
+train_dst_fft = np.fft.fft(train_dst, axis = 1)/ n2_1
+test_dst_fft = np.fft.fft(test_dst, axis = 1)/ n2_2
 
 # fill = 0
 train_scr_fft = np.nan_to_num(train_scr_fft, copy=False)
@@ -27,6 +33,21 @@ test_scr_fft = np.nan_to_num(test_scr_fft, copy=False)
 
 train_dst_fft = np.nan_to_num(train_dst_fft, copy=False)
 test_dst_fft = np.nan_to_num(test_dst_fft, copy=False)
+
+# abs
+train_scr_fft = np.abs(train_scr_fft)
+test_scr_fft = np.abs(test_scr_fft)
+
+train_dst_fft = np.abs(train_dst_fft)
+test_dst_fft = np.abs(test_dst_fft)
+
+# one_side
+train_scr_fft = train_dst_fft[range(int(n1_1 / 2))]
+train_scr_fft = train_dst_fft[range(int(n1_2 / 2))]
+
+train_scr_fft = train_dst_fft[range(int(n2_1 / 2))]
+train_scr_fft = train_dst_fft[range(int(n2_2 / 2))]
+
 
 # save
 np.save('./dacon/comp1/data/train_scr_fft.npy', arr= train_scr_fft)
