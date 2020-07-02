8b2d339918003411c842c5b20a76b6d874d1898a
Jain <zaiin4050@gmail.com>
2020-07-02 19:08:54 +0900

    람다 곱해줌

--- keras/keras110_6_selu.py
+++ keras/keras110_6_selu.py
@@ -1,18 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def selu(x, a =1.67326):
+def selu(x, l = 1.0507, a =1.67326):
     y_list = []
     for x in x:
-        if x > 0:
+        if x >= 0:
             y = x
-        if x <= 0:
-            y = a*(np.exp(x) - 1)
+        if x < 0:
+            y = l*a*(np.exp(x) - 1)
         y_list.append(y)
     return y_list
 
-def selu(x, a = 1.6732):
-    return list(map(lambda x : x if x > 0 else a*(np.exp(x)-1), x))
+def selu(x, l =1.0507, a = 1.6732):
+    return list(map(lambda x : x if x >= 0 else l*a*(np.exp(x)-1), x))
 
 x = np.arange(-5, 5, 0.1)
 y = selu(x)
