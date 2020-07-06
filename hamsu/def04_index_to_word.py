35eb09001f9a0659d7b18e242e03df611e864848
Jain <zaiin4050@gmail.com>
2020-07-06 15:49:48 +0900

    주석 추가

--- hamsu/def04_index_to_word.py
+++ hamsu/def04_index_to_word.py
@@ -1,12 +1,16 @@
 def index_to_word(dataset, data):
     word_to_index = dataset.get_word_index()
     index_to_word = {}
+    # print(word_to_index)                   # {'단어' : index}
+
     for key, value in word_to_index.items():
         index_to_word[value] = key
-    
+    # print(index_to_word)                   # {'index' : 단어}
+ 
     text = ' '.join([index_to_word[index] for index in data])
+            # .join : 리스트를 특정 구분자를 포함해 문자열로 변환
+
     print(text)
-    
     return text
 
 '''
