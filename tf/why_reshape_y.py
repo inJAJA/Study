33381008260c552ab7cd7fe59d80d6397eece539
Jain <zaiin4050@gmail.com>
2020-07-09 09:27:31 +0900

    결과 주석

--- tf/why_reshape_y.py
+++ tf/why_reshape_y.py
@@ -5,10 +5,16 @@ y1 = np.transpose([[1, 2, 3]])
 
 y2 = np.array([1, 2, 3])
 
-print(h.shape)
-print(y1.shape)
-print(y2.shape)
+print(h.shape)    # (3, 1)
+print(y1.shape)   # (3, 1)
+print(y2.shape)   # (3,)
 
 
 print(h - y1)
+# [[1]
+#  [2]
+#  [3]]
 print(h - y2)
+# [[ 1  0 -1]
+#  [ 3  2  1]
+#  [ 5  4  3]]
