2b454f9abd83bdf3ff61351b0e9c19a62634f408
Jain <zaiin4050@gmail.com>
2020-07-13 15:22:21 +0900

    2020.07.13

---
+++ flask/fk04_response.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask import make_response
+
+@app.route('/')
+def index():
+    response = make_response('<h1> 잘 다라 치시오!! </h1>')
+    response.set_cookie('answer','42')
+    return response
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=False)
