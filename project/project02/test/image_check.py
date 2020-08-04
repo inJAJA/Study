be9b9076f1fb66ec9474004cf3deb5a7e0df6221
Zain <zaiin4050@gmail.com>
2020-08-04 14:56:10 +0900

    key값 확인

--- project/project02/test/image_check.py
+++ project/project02/test/image_check.py
@@ -1,8 +1,11 @@
 import h5py
 import cv2
 
-with h5py.File('C:/Users/bitcamp\Desktop/breed_add/hdf5/pekingese.hdf5', 'r') as f:
-    img = f['pekingese'][100]
+with h5py.File('C:/Users/bitcamp\Desktop/breed_add/hdf5/face_Doberman.hdf5', 'a') as f:
+
+    print('key값 :',f.keys())
+
+    img = f['doberman'][150]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imshow('img', img)
     cv2.waitKey(0)
