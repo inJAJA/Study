9eaba7e99d7bae1061d8bd505212cd2ee6fc0f93
Zain <zaiin4050@gmail.com>
2020-08-05 09:18:06 +0900

    hdf5파일의 이미지 확인

--- project/project02/test/image_check.py
+++ project/project02/test/image_check.py
@@ -1,11 +1,12 @@
 import h5py
 import cv2
 
-with h5py.File('C:/Users/bitcamp\Desktop/breed_add/hdf5/face_Doberman.hdf5', 'a') as f:
+# with h5py.File('C:/Users/bitcamp\Desktop/breed_add/hdf5/face_Doberman.hdf5', 'a') as f:
+with h5py.File('D:/data/face_Italian_Greyhound_part2.hdf5', 'a') as f:
 
     print('key값 :',f.keys())
 
-    img = f['doberman'][150]
+    img = f['Italian_Greyhound'][50]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imshow('img', img)
     cv2.waitKey(0)
