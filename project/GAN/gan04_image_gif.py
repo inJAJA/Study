8823fd3956959730f0c1669355037237bd43bcef
Zain <zaiin4050@gmail.com>
2020-08-06 16:29:24 +0900

    fps 조정

--- project/GAN/gan04_image_gif.py
+++ project/GAN/gan04_image_gif.py
@@ -8,8 +8,8 @@ import os
 
 filename = os.listdir("D:/Study/project/GAN/result/conv2d")
 f = Tcl().call('lsort', '-dict', filename)                  # filename sort
-print(f)
+# print(f)
 
 path = [f"D:/Study/project/GAN/result/conv2d/{i}" for i in f]
 paths = [ Image.open(i) for i in path]
-imageio.mimsave('D:/Study/project/GAN/result/pngs/test.gif', paths, fps=0.9)
+imageio.mimsave('D:/Study/project/GAN/result/pngs/test.gif', paths, fps=2)
