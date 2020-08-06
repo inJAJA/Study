99407f93bb642e060885a0ee97e5a23478231445
Zain <zaiin4050@gmail.com>
2020-08-06 16:49:05 +0900

    path수정

--- project/GAN/gan04_image_gif.py
+++ project/GAN/gan04_image_gif.py
@@ -6,10 +6,12 @@ import matplotlib.image as mpimg
 from tkinter import Tcl
 import os
 
-filename = os.listdir("D:/Study/project/GAN/result/conv2d")
+file_dir = "D:/Study/project/GAN/result/lsgan"
+
+filename = os.listdir(file_dir)
 f = Tcl().call('lsort', '-dict', filename)                  # filename sort
 # print(f)
 
-path = [f"D:/Study/project/GAN/result/conv2d/{i}" for i in f]
+path = [file_dir+f"/{i}" for i in f]
 paths = [ Image.open(i) for i in path]
-imageio.mimsave('D:/Study/project/GAN/result/pngs/test.gif', paths, fps=2)
+imageio.mimsave('D:/Study/project/GAN/result/pngs/lsgan_0.0002.gif', paths, fps=2)
