ac81e61b33a8ccbf9b4baf69ce8064f1eb2d034f
Zain <zaiin4050@gmail.com>
2020-08-11 09:44:46 +0900

    2020.08.11

--- project/GAN/gan06_FreezeD_model.py
+++ project/GAN/gan06_FreezeD_model.py
@@ -29,14 +29,13 @@ class EqualLR:
                                                                     # .numel() : size의 모든 인수 곱하기
         return weight * sqrt(2 / fan_in)
 
-    @staticmethod   # 정적 메소드
+                    # 정적 메소드
                     # class에서 직접 접근할 수 있는 메소드
-                    # self를 통한 접근 x
+    @staticmethod   # self를 통한 접근 x
     def apply(module, name):
         fn = EqualLR(name)
 
         weight = getattr(module, name)
-        
         del module._parameters[name]
         module.register_parameter(name + '_orig', nn.Paranmeter(weight.data))
         module.register_forward_pre_hook(fn)
@@ -47,10 +46,10 @@ class EqualLR:
         weight = self.compute_weight(module)
         setattr(module, self.name, weight)      # setattr(object, name, value)  : object에 존재하는 속성의 값 설정
 
-    def equal_lr(module, name='weight'):
-        EqualLR.apply(module, name)
+def equal_lr(module, name='weight'):
+    EqualLR.apply(module, name)
 
-        return module
+    return module
 
 
 class FusedUpsample(nn.Module):
@@ -246,3 +245,351 @@ class AdaptiveInstanceNorm(nn.Module):
         out = gamma * out + beta
 
         return out      # 284 line 
+
+
+class NoiseInjection(nn.Module):
+    def __init__(self, channel):
+        super().__init__()
+
+        self.weight = nn.Parameter(torch.zeros(1, channel, 1, 1))
+
+    def forward(self, image, noise):
+        return image + self.weight * noise
+
+
+class ConstantInput(nn.Module):
+    def __init__(self, channel, size = 4):
+        super().__init__()
+
+        self.input = nn.Parameter(torch.randn(1, channel, size, size))
+
+    def forward(self, input):
+        batch = input.shape[0]
+        out = self.input.repeat(batch, 1, 1, 1) # .repeat : 숫자를 반복하여 차원 늘림
+
+        return out
+
+class StyleConvBlock(nn.Module):
+    def __init__(
+        self,
+        in_channel,
+        out_channel,
+        kernel_size = 3,
+        padding = 1,
+        style_dim = 512,
+        initial = False,
+        upsample = False,
+        fused = False,
+    ):
+        super().__init__()
+
+        if initial:
+            self.conv1 = ConstantInput(in_channel)
+
+        else:
+            if upsample:
+                if fused:
+                    self.conv1 = nn.Sequential(
+                        FusedUpsample(
+                            in_channel, out_channel, kernel_size, padding = padding
+                        ),
+                        Blur(out_channel),
+                    )
+
+                else:
+                    self.conv1 = nn.Sequential(
+                        FusedUpsample(
+                            in_channel, out_channel, kernel_size, padding = padding
+                        ),
+                        Blur(out_channel),
+                    )
+
+            else:
+                self.conv1 = EqualConv2d(
+                    in_channel, out_channel, kernel_size, padding = padding
+                )
+
+        self.noise1 = equal_lr(NoiseInjection(out_channel))
+        self.adain1 = AdaptiveInstanceNorm(out_channel, style_dim)
+        self.lrelu1 = nn.LeakyReLU(0.2)
+
+        self.conv2 = EqualConv2d(out_channel, out_channel, kernel_size, padding = padding)
+        self.noise2 = equal_lr(NoiseInjection(out_channel))
+        self.adain2 = AdaptiveInstanceNorm(out_channel, style_dim)
+        self.lrelu2 = nn.LeakyReLU(0.2)
+
+    def forward(slef, input, style, noise):
+        out = self.conv1(input)
+        out = self.noise1(out, noise)
+        out = self.lrelu1(out)
+        out = self.adain1(out, style)
+
+        out = self.conv2(out)
+        out = self.noise2(out, noise)
+        out = self.lrelu2(out)
+        out = self.adain2(out, style)
+
+        return out
+class Generator(nn.Module):
+    def __init__(self, code_dim, fused=True):
+        super().__init__()
+
+        self.progression = nn.ModuleList(                       # nn.ModuleList : foward method가 필요 X
+            [
+                StyledConvBlock(512, 512, 3, 1, initial=True),  # 4
+                StyledConvBlock(512, 512, 3, 1, upsample=True),  # 8
+                StyledConvBlock(512, 512, 3, 1, upsample=True),  # 16
+                StyledConvBlock(512, 512, 3, 1, upsample=True),  # 32
+                StyledConvBlock(512, 256, 3, 1, upsample=True),  # 64
+                StyledConvBlock(256, 128, 3, 1, upsample=True, fused=fused),  # 128
+                StyledConvBlock(128, 64, 3, 1, upsample=True, fused=fused),  # 256
+                StyledConvBlock(64, 32, 3, 1, upsample=True, fused=fused),  # 512
+                StyledConvBlock(32, 16, 3, 1, upsample=True, fused=fused),  # 1024
+            ]
+        )
+
+        self.to_rgb = nn.ModuleList(
+            [
+                EqualConv2d(512, 3, 1),
+                EqualConv2d(512, 3, 1),
+                EqualConv2d(512, 3, 1),
+                EqualConv2d(512, 3, 1),
+                EqualConv2d(256, 3, 1),
+                EqualConv2d(128, 3, 1),
+                EqualConv2d(64, 3, 1),
+                EqualConv2d(32, 3, 1),
+                EqualConv2d(16, 3, 1),
+            ]
+        )
+
+        # self.blur = Blur()
+        # ''' 복붙함 '''
+        
+    def forward(self, style, noise, step=0, alpha=-1, mixing_range=(-1, -1)):
+        out = noise[0]
+
+        if len(style) < 2:
+            inject_index = [len(self.progression) + 1]
+
+        else:
+            inject_index = random.sample(list(range(step)), len(style) - 1)
+
+        crossover = 0
+
+        for i, (conv, to_rgb) in enumerate(zip(self.progression, self.to_rgb)):
+            if mixing_range == (-1, -1):
+                if crossover < len(inject_index) and i > inject_index[crossover]:
+                    crossover = min(crossover + 1, len(style))
+
+                style_step = style[crossover]
+
+            else:
+                if mixing_range[0] <= i <= mixing_range[1]:
+                    style_step = style[1]
+
+                else:
+                    style_step = style[0]
+
+            if i > 0 and step > 0:
+                out_prev = out
+                
+            out = conv(out, style_step, noise[i])
+
+            if i == step:
+                out = to_rgb(out)
+
+                if i > 0 and 0 <= alpha < 1:
+                    skip_rgb = self.to_rgb[i - 1](out_prev)
+                    skip_rgb = F.interpolate(skip_rgb, scale_factor=2, mode='nearest')
+                    out = (1 - alpha) * skip_rgb + alpha * out
+
+                break
+
+        return out
+
+
+class StyledGenerator(nn.Module):
+    def __init__(self, code_dim=512, n_mlp=8, dataset_size=0, embed_dim=512):
+        super().__init__()
+
+        self.generator = Generator(code_dim)
+
+        layers = [PixelNorm()]
+        for i in range(n_mlp):
+            layers.append(EqualLinear(code_dim, code_dim))
+            layers.append(nn.LeakyReLU(0.2))
+
+        self.style = nn.Sequential(*layers)
+
+        if dataset_size > 0:
+            self.embedding = nn.Embedding(dataset_size, embed_dim)
+            self.embedding.from_pretrained(torch.zeros(dataset_size, embed_dim), freeze=False)
+
+    def forward(
+        self,
+        input,
+        noise=None,
+        step=0,
+        alpha=-1,
+        mean_style=None,
+        style_weight=0,
+        mixing_range=(-1, -1),
+    ):
+        styles = []
+        if type(input) not in (list, tuple):
+            input = [input]
+
+        for i in input:
+            styles.append(self.style(i))
+
+        batch = input[0].shape[0]
+
+        if noise is None:
+            noise = []
+
+            for i in range(step + 1):
+                size = 4 * 2 ** i
+                noise.append(torch.randn(batch, 1, size, size, device=input[0].device))
+
+        if mean_style is not None:
+            styles_norm = []
+
+            for style in styles:
+                styles_norm.append(mean_style + style_weight * (style - mean_style))
+
+            styles = styles_norm
+
+        return self.generator(styles, noise, step, alpha, mixing_range=mixing_range)
+
+    def mean_style(self, input):
+        style = self.style(input).mean(0, keepdim=True)
+
+        return style
+
+
+class Discriminator(nn.Module):
+    def __init__(self, fused=True, from_rgb_activate=False):
+        super().__init__()
+
+        self.progression = nn.ModuleList(
+            [
+                ConvBlock(16, 32, 3, 1, downsample=True, fused=fused),  # 512
+                ConvBlock(32, 64, 3, 1, downsample=True, fused=fused),  # 256
+                ConvBlock(64, 128, 3, 1, downsample=True, fused=fused),  # 128
+                ConvBlock(128, 256, 3, 1, downsample=True, fused=fused),  # 64
+                ConvBlock(256, 512, 3, 1, downsample=True),  # 32
+                ConvBlock(512, 512, 3, 1, downsample=True),  # 16
+                ConvBlock(512, 512, 3, 1, downsample=True),  # 8
+                ConvBlock(512, 512, 3, 1, downsample=True),  # 4
+                ConvBlock(513, 512, 3, 1, 4, 0),
+            ]
+        )
+
+        def make_from_rgb(out_channel):
+            if from_rgb_activate:
+                return nn.Sequential(EqualConv2d(3, out_channel, 1), nn.LeakyReLU(0.2))
+
+            else:
+                return EqualConv2d(3, out_channel, 1)
+
+        self.from_rgb = nn.ModuleList(
+            [
+                make_from_rgb(16),
+                make_from_rgb(32),
+                make_from_rgb(64),
+                make_from_rgb(128),
+                make_from_rgb(256),
+                make_from_rgb(512),
+                make_from_rgb(512),
+                make_from_rgb(512),
+                make_from_rgb(512),
+            ]
+        )
+
+        # self.blur = Blur()
+
+        self.n_layer = len(self.progression)
+
+        self.linear = EqualLinear(512, 1)
+
+    def forward(self, input, step=0, alpha=-1, get_feature=False, feature_loc=0, linearize=True):
+        for i in range(step, -1, -1):
+            index = self.n_layer - i - 1
+
+            if i == step:
+                out = self.from_rgb[index](input)
+
+            if i == 0:
+                out_std = torch.sqrt(out.var(0, unbiased=False) + 1e-8)
+                mean_std = out_std.mean()
+                mean_std = mean_std.expand(out.size(0), 1, 4, 4)
+                out = torch.cat([out, mean_std], 1)
+
+            out = self.progression[index](out)
+
+            if i > 0:
+                if i == step and 0 <= alpha < 1:
+                    skip_rgb = F.avg_pool2d(input, 2)
+                    skip_rgb = self.from_rgb[index + 1](skip_rgb)
+
+                    out = (1 - alpha) * skip_rgb + alpha * out
+
+            if get_feature and i == feature_loc:  # feature_loc is inverse order (0 is highest)
+                if linearize:
+                    return out.view(len(out), -1)
+                else:
+                    return out
+
+        out = out.squeeze(2).squeeze(2)
+        out = self.linear(out)
+
+        return out
+
+
+    def get_all_features(self, input, step=0, alpha=-1, linearize=True):
+        feats = []
+
+        for i in range(step, -1, -1):
+            index = self.n_layer - i - 1
+
+            if i == step:
+                out = self.from_rgb[index](input)
+
+            if i == 0:
+                out_std = torch.sqrt(out.var(0, unbiased=False) + 1e-8)
+                mean_std = out_std.mean()
+                mean_std = mean_std.expand(out.size(0), 1, 4, 4)
+                out = torch.cat([out, mean_std], 1)
+
+            out = self.progression[index](out)
+
+            if i > 0:
+                if i == step and 0 <= alpha < 1:
+                    skip_rgb = F.avg_pool2d(input, 2)
+                    skip_rgb = self.from_rgb[index + 1](skip_rgb)
+
+                    out = (1 - alpha) * skip_rgb + alpha * out
+
+            if linearize:
+                feats.append(out.view(len(out), -1))
+            else:
+                feats.append(out)
+
+        return feats
+
+
+class Miner(nn.Module):
+    def __init__(self, code_dim=512):
+        super().__init__()
+        self.transform = nn.Sequential(
+            nn.Linear(code_dim, code_dim),
+            nn.ReLU(),
+            nn.Linear(code_dim, code_dim),
+        )
+
+    def forward(self, input):
+        if type(input) not in (list, tuple):
+            input = [input]
+
+        output = [self.transform(i) for i in input]
+        return output
