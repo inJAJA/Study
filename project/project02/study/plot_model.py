c6f3435b64e6fd91b7bebcc40f7bfc68172a5bad
Zain <zaiin4050@gmail.com>
2020-08-26 17:07:17 +0900

    2020.08.26

---
+++ project/project02/study/plot_model.py
@@ -0,0 +1,20 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(10, input_shape = (1, )))
+model.add(Dense(5))
+model.add(Dense(1))
+
+model.summary()
+
+from keras.utils import plot_model
+plot_model(model, "D:/model.png", show_shapes=True)
+'''
+1. pip install pydot
+2. pip install pydotplus
+2. pip install graphviz
+
+# 에러 발생시 다음의 링크 참고
+ : https://ndlessrain.tistory.com/entry/graphviz-pathpydot-failed-to-call-GraphViz
+'''
