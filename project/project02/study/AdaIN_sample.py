3d2cf48a5578bee32270f872f9e3b3b2ce68fe37
GooJaIn <zaiin4050@gmail.com>
2020-08-28 22:00:33 +0900

    주석 추가

--- project/project02/study/AdaIN_sample.py
+++ project/project02/study/AdaIN_sample.py
@@ -4,13 +4,16 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# load Image
 x = cv2.imread('D:\image/image4.jpg')
 x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
 x = cv2.resize(x, dsize = (750, 400), interpolation = cv2.INTER_LINEAR)/255.
+x = x.reshape(-1, 400, 750, 3)
 
-y = cv2.imread('D:/image/image3.jpg')
+y = cv2.imread('D:/image/image2.jpg')
 y = cv2.cvtColor(y, cv2.COLOR_BGR2RGB)
 y = cv2.resize(y, dsize=(750, 400), interpolation = cv2.INTER_LINEAR)/ 255.
+y = y.reshape(-1, 400, 750, 3)
 
 # keras backend를 사용하기 위해서 numpy -> tensor로 변환
 x = tf.convert_to_tensor(x, np.float32)
@@ -20,7 +23,7 @@ axis = -1
 epsilon=1e-3
 
 input_shape = K.int_shape(x)
-# print(input_shape)              # (400, 750, 3)
+# print(input_shape)               # (400, 750, 3)
 
 reduction_axes = list(range(0, len(input_shape)))
 print(reduction_axes)              # [0, 1, 2]
@@ -33,17 +36,34 @@ if axis is not None:
 
 del reduction_axes[0]
 
+print(reduction_axes)
+
+gamma = K.std(y, reduction_axes, keepdims=True) + epsilon
+beta = K.mean(y, reduction_axes, keepdims=True)
+print(beta.shape)                   # (1, 1, 1, 3)
+print(gamma.shape)                  # (1, 1, 1, 3)
+
 # Adain
 mean = K.mean(x, reduction_axes, keepdims=True)
 stddev = K.std(x, reduction_axes, keepdims=True) + epsilon
 normed = (x - mean) / stddev
 
+print(mean.shape)                   # (1, 1, 1, 3)
+print(stddev.shape)                 # (1, 1, 1, 3)
+print(normed.shape)                 # (1, 400, 750, 3)
+
 # result
 image = normed * gamma + beta
-print(image.shape)
+print(image.shape)                  # (1, 400, 750, 3)
 
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+normed = K.reshape(normed, (400, 750, 3))
+
+x = K.reshape(x, (400, 750, 3))
+y = K.reshape(y, (400, 750, 3))
+image = K.reshape(image, (400, 750, 3))
+
+# show Image
 fig, axes = plt.subplots(1, 3)
 
 axes[0].imshow(x)
