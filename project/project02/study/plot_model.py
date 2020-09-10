a46b6b89306a21d68492c8c22ab3ecea1f9bed5f
inJAJA <64456018+inJAJA@users.noreply.github.com>
2020-09-10 14:20:09 +0900

    Update plot_model.py
    pydot install in Linux

--- project/project02/study/plot_model.py
+++ project/project02/study/plot_model.py
@@ -11,10 +11,15 @@ model.summary()
 from keras.utils import plot_model
 plot_model(model, "D:/model.png", show_shapes=True)
 '''
+# Window
 1. pip install pydot
 2. pip install pydotplus
 2. pip install graphviz
 
 # 에러 발생시 다음의 링크 참고
  : https://ndlessrain.tistory.com/entry/graphviz-pathpydot-failed-to-call-GraphViz
+ 
+# Linux
+1. pip install pydot>=1.2.4
+2. sudo apt-get install graphviz
 '''
